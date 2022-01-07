90d901a2bc7e5de04a0e2e6d245b5302a3119ca4
ics4u-1-2021 <94392955+ics4u-1-2021@users.noreply.github.com>
2022-01-07 17:33:05 +0000

    init

---
+++ js/game.js
@@ -0,0 +1,30 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Phaser3 configuration file
+
+
+//* Game scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
